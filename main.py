b2d4a767db8776ed2c4908e4e58aa46b74615ad6
Gkatziaris Pantelis <gatziaris.pantelis@hotmail.com>
2019-10-22 13:47:27 +0300

    Added simple mail proofing to avoid files with @ in the middle of their names.

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from collections import deque
 import re
 import pandas as pd
 
-
-linker = '' # google search url 
+trusted_ = ['gr, com, gov, net, org, info, nfo, be']
+linker = '' # google search url
 new_urls = deque([linker])
 mail_db = pd.DataFrame(columns=['emails', 'from'])
 counter = 0
@@ -40,29 +40,30 @@ while len(new_urls):
 
     # extract all email addresses and add them into the resulting set
     new_emails = set(re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", response.text, re.I))
-    emails.update(new_emails)
-    if new_emails not in mail_db.values:
-        try:
-            mail_db.loc[counter, 'emails'] = emails.pop()
-        except Exception as err:
-            pass
-        mail_db.loc[counter, 'from'] = str(url).replace('"', '')
-        counter += 1
-        mail_db.dropna(subset=['emails'], inplace=True)
-        mail_db.drop_duplicates(subset=['emails'], inplace=True)
-        mail_db.to_csv('test.csv', sep=';', encoding='utf-8', index=False)
-    # create a beutiful soup for the html document
-    soup = BeautifulSoup(response.text, features="html.parser")
+    if new_emails.split('.')[-1].lower() in trusted_:       # email proofing
+        emails.update(new_emails)
+        if new_emails not in mail_db.values:
+            try:
+                mail_db.loc[counter, 'emails'] = emails.pop()
+            except Exception as err:
+                pass
+            mail_db.loc[counter, 'from'] = str(url).replace('"', '')
+            counter += 1
+            mail_db.dropna(subset=['emails'], inplace=True)
+            mail_db.drop_duplicates(subset=['emails'], inplace=True)
+            mail_db.to_csv('test.csv', sep=';', encoding='utf-8', index=False)
+        # create a beutiful soup for the html document
+        soup = BeautifulSoup(response.text, features="html.parser")
 
-    # find and process all the anchors in the document
-    for anchor in soup.find_all("a"):
-        # extract link url from the anchor
-        link = anchor.attrs["href"] if "href" in anchor.attrs else ''
-        # resolve relative links
-        if link.startswith('/'):
-            link = base_url + link
-        elif not link.startswith('http'):
-            link = path + link
-        # add the new url to the queue if it was not enqueued nor processed yet
-        if not link in new_urls and not link in processed_urls:
-            new_urls.append(link)
+        # find and process all the anchors in the document
+        for anchor in soup.find_all("a"):
+            # extract link url from the anchor
+            link = anchor.attrs["href"] if "href" in anchor.attrs else ''
+            # resolve relative links
+            if link.startswith('/'):
+                link = base_url + link
+            elif not link.startswith('http'):
+                link = path + link
+            # add the new url to the queue if it was not enqueued nor processed yet
+            if not link in new_urls and not link in processed_urls:
+                new_urls.append(link)
